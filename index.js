e0da8b02d3a3adca004e7815a35314cf58501504
Biswaranjan Rout <biswaranjan.rout@xceedance.com>
2018-02-23 10:59:13 +0530

    Added new methods

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res) {
+restService.post("/turbo", function(req, res) {
   var speech =
     req.body.result &&
     req.body.result.parameters &&

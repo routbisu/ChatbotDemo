316e7aeb4175d8b95b7abe4cdbb8ca84cee1bcbb
Biswaranjan Rout <biswaranjan.rout@xceedance.com>
2018-03-02 15:48:31 +0530

    Fixed issues

--- services/payment-api-service.js
+++ services/payment-api-service.js
@@ -2,7 +2,8 @@
 const config            = require('../app.config');
 const nodeRest          = require('node-rest-client').Client;
 const nodeRestClient    = new nodeRest();
-const commonServices = require('./common');
+const commonServices    = require('./common');
+const log               = require('node-file-logger');
 
 const turboAPIBaseURL = config.paymentAPI.BaseURL;
 
@@ -15,6 +16,7 @@ module.exports = {
 
         // Default speech.
         let defaultSpeech = 'I am not able to find the information currently. Please try again.';
+        let policyNotFound = 'I was not able to find any data for this policy number. Please try again.';
 
         if(params['policynumber']) {
             let url = turboAPIBaseURL + 'Chatbot/GetPolicyDetails?PolicyNumber=' 
@@ -23,21 +25,30 @@ module.exports = {
             nodeRestClient.get(url, function (data, response) {
 
                 if(data) {
+                    log.Info(data);
                     if(data.Error == 0) {
                         let customerName = false;
                         if(data.CustomerContactDetails) {
                             customerName = data.CustomerContactDetails.FirstName;
                         } 
 
-                        let speech = customerName ? ('Hi ' + customerName + '.') : '';
-                        speech += data.ProductType = ' Policy ' + data.PolicyNumber + ' is valid till ' 
-                            + data.PolicyEndDate + ' with total premium $' + data.TotalPremium; 
+                        let speech;
+
+                        if(data.CustomerID) {
+                            speech = customerName ? ('Hi ' + customerName + '.') : '';
+                            speech += data.ProductType = ' Policy ' + data.PolicyNumber + ' is valid till ' 
+                                + data.PolicyEndDate + ' with total premium $' + data.TotalPremium; 
+                        } else {
+                            speech = policyNotFound;
+                        }                       
 
                         commonServices.SendResponse(res, speech);
+
+                    } else {
+                        commonServices.SendResponse(res, policyNotFound);
                     }
                 } else {
-                    commonServices.SendResponse(res, 'The policy number you entered is invalid, can you please try again?');
-                    
+                    commonServices.SendResponse(res, policyNotFound);               
                 }
             });
         } else {

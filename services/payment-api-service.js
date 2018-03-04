194c76b292bce3a6f1cdde417fe2772bef6d4af6
Biswaranjan Rout <biswaranjan.rout@xceedance.com>
2018-03-04 10:25:59 +0530

    Fixed issues

--- services/payment-api-service.js
+++ services/payment-api-service.js
@@ -9,169 +9,6 @@ const turboAPIBaseURL = config.paymentAPI.BaseURL;
 
 module.exports = {
 
-    /**
-     * @param {*} params params sent from dialog flow
-     */
-    ProcessRequestOld: function(res, result) {
-
-        // Default speech.
-        let defaultSpeech = 'I am not able to find the information currently. Please try again.';
-        let policyNotFound = 'I was not able to find any data for this policy number. Please try again.';
-        let otpEmailed = 'Please enter the one time password emailed to you at: ';
-        let correctOtp = 'Your OTP has been verified.';
-        let wrongOtp = 'The password you entered is incorrect. Please enter again.';
-
-        if(params['policynumber']) {
-
-            let url = turboAPIBaseURL + 'Chatbot/GetCustomerDetails?PolicyNumber=' 
-                + params['policynumber'];
-
-            nodeRestClient.get(url, function (data, response) {
-
-                log.Debug(data, 'PaymentAPI', 'Check policy number');
-
-                let validPolicy = false;
-                if(data) {
-                    if(data.Error == 0) {
-                        if(data.Email) {
-                            validPolicy = true;
-                        }
-                    }
-                }
-
-                if(validPolicy) {
-                    // Send OTP  
-                    let otpUrl = turboAPIBaseURL + 'Chatbot/SendOTP';
-
-                    let args = {
-                        data: {
-                            Name: data.Name,
-                            Email: data.Email,
-                            PolicyNumber: data.PolicyNumber
-                        },
-                        headers: { "Content-Type": "application/json" }
-                    };
-
-                    nodeRestClient.post(otpUrl, args, function (data, response) {
-                        if(data) {
-                            log.Debug(data, 'PaymentAPI', 'Send OTP');
-
-                            if(data.Error == 0) {
-
-                                let contextOut = [
-                                    {
-                                        name: "otp", 
-                                        lifespan: 500, 
-                                        parameters : 
-                                        { 
-                                            serverotp: data.EncryptedOTP 
-                                        }
-                                    }
-                                ];
-
-                                let speech = otpEmailed + args.data.Email;
-                                commonServices.SendResponse(res, speech, contextOut);
-                            
-                            } else {
-
-                                let followupEvent = {
-                                    name: 'askpolicynumber',
-                                    data: {
-                                        serverotp: data.EncryptedOTP 
-                                    }
-                                };
-
-                                commonServices.SendResponse(res, policyNotFound, null, followupEvent);
-                            }
-                        } else {
-
-                            let followupEvent = {
-                                name: 'askpolicynumber',
-                                data: {
-                                    serverotp: data.EncryptedOTP 
-                                }
-                            };
-
-                            commonServices.SendResponse(res, policyNotFound, null, followupEvent);
-                        }
-                    });
-                } else {
-                    let followupEvent = {
-                        name: 'askpolicynumber',
-                        data: {
-                            serverotp: data.EncryptedOTP 
-                        }
-                    };
-
-                    commonServices.SendResponse(res, policyNotFound, null, followupEvent);
-                }
-            });
-
-        } 
-        else if(params['userotp']) {
-            // Find OTP context
-            let otpContext = null;
-
-            for(i = 0; i < contexts.length; i++) {
-                if(contexts[i].name == "otp") {
-                    otpContext = contexts[i];
-                    break;
-                }
-            }
-
-            if(otpContext.parameters.serverotp.toLowerCase() === params['userotp'].toLowerCase()) {
-                //commonServices.SendResponse(res, correctOtp);
-
-                // Get Policy information
-                let policyInfo;
-                for(i = 0; i < contexts.length; i++) {
-                    if(contexts[i].name == "policyinfo") {
-                        policyInfo = contexts[i];
-                        break;
-                    }
-                }
-
-                let policyNumber = policyInfo.parameters.policynumber;
-                let url = turboAPIBaseURL + 'Chatbot/GetPolicyDetails?PolicyNumber=' + policyNumber;
-                nodeRestClient.get(url, function (data, response) {
-                    if(data) {
-                        log.Debug(data);
-                        if(data.Error == 0) {
-                            let customerName = false;            
-                            if(data.CustomerContactDetails) {
-                                customerName = data.CustomerContactDetails.FirstName + ' ' 
-                                    + data.CustomerContactDetails.LastName;
-                            } 
-            
-                            let speech;
-            
-                            if(data.CustomerID) {
-                                speech = data.ProductType + ' Policy ' + data.PolicyNumber + ' with Insured name ' 
-                                    + customerName + ' is valid till ' + data.PolicyEndDate + ' with total premium $' 
-                                    + data.TotalPremium ;
-                            } else {
-                                speech = policyNotFound;
-                            }                       
-    
-                            commonServices.SendResponse(res, speech);
-    
-                        } else {
-                            commonServices.SendResponse(res, policyNotFound);
-                        }
-                    } else {
-                        commonServices.SendResponse(res, policyNotFound);               
-                    }
-                });
-
-            } else {
-                commonServices.SendResponse(res, wrongOtp);
-            }
-        }
-        else {
-            commonServices.SendResponse(res, defaultSpeech);
-        }
-    },
-
     // Process requests - interact with Payment API
     ProcessRequest: function(res, result) {
 
@@ -360,7 +197,10 @@ module.exports = {
                 else  {
                     // Send user back to authentication intent
                     let followupEvent = {
-                        name: 'authenticate'
+                        name: 'authenticate',
+                        data: {
+                            policynumber: result.parameters.pnumgetpolicydetails
+                        }
                     };
 
                     // Send last event details
@@ -378,6 +218,8 @@ module.exports = {
                     commonServices.SendResponse(res, '', contextOut, followupEvent);
                 }
 
+            } else if(result.action == 'get') {
+                commonServices.SendResponse(res, SPEECH.default);
             } else {
                 commonServices.SendResponse(res, SPEECH.default);
             }
